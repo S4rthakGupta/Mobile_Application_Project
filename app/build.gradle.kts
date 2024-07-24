d286f803a8e1d58dc471fff79e221d270af38e13
S4rthak Gupta <s4rthak.gupta@gmail.com>
2024-07-24 17:40:13 -0400

    commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,18 +22,28 @@ android {
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.recyclerview) // Add RecyclerView
+    implementation(libs.cardview) // Add CardView
+    implementation(libs.room.runtime) // Add Room Runtime
+    kapt(libs.room.compiler) // Add Room Compiler
+    implementation(libs.picasso) // Add Picasso
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
